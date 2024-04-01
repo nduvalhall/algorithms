492f03e8d4509297f73a36b6d36f22a59f92e859
nduvalhall <nduvalhall@gmail.com>
2024-04-01 12:15:34 +0100

    wrote tests

--- binary_search/src/lib.rs
+++ binary_search/src/lib.rs
@@ -1,21 +1,56 @@
 #[allow(dead_code)]
-fn binary_search(target: i32, array: &[i32]) -> Option<i32> {
-    println!("Binary search: searching for {}", target);
-
-    let index = 0;
-    // do search
-
-    println!("Found {} at index {}", target, index);
-
-    Some(index)
+fn binary_search(_target: usize, _slice: &[usize]) -> Option<usize> {
+    None
 }
 
 #[test]
-fn it_works() {
-    let target = 12;
-    let array = [1, 3, 6, 12, 24, 35];
+fn even_length_array() {
+    let slice = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] as [usize; 10];
 
-    let index = binary_search(target, &array);
+    for (i, &target) in slice.iter().enumerate() {
+        let result = binary_search(target, &slice);
+        assert!(result.is_some(), "target {} was not found!", target);
+        assert_eq!(result.unwrap(), i, "index is incorrect!");
+    }
 
-    assert!(index.is_some())
+    let target = 0;
+    let result = binary_search(target, &slice);
+    assert!(result.is_none(), "target was found!");
+}
+
+#[test]
+fn odd_length_array() {
+    let slice = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110] as [usize; 11];
+
+    for (i, &target) in slice.iter().enumerate() {
+        let result = binary_search(target, &slice);
+        assert!(result.is_some(), "target {} was not found!", target);
+        assert_eq!(result.unwrap(), i, "index is incorrect!");
+    }
+
+    let target = 0;
+    let result = binary_search(target, &slice);
+    assert!(result.is_none(), "target was found!");
+}
+
+#[test]
+fn one_item_array() {
+    let slice = [10] as [usize; 1];
+    let target = 10;
+    let result = binary_search(target, &slice);
+
+    assert!(result.is_some(), "target 0 was not found!");
+    assert_eq!(result.unwrap(), target, "index is incorrect!");
+
+    let target = 0;
+    let result = binary_search(target, &slice);
+    assert!(result.is_none(), "target was found!");
+}
+
+#[test]
+fn empty_array() {
+    let slice = [];
+    let target = 0;
+    let result = binary_search(target, &slice);
+    assert!(result.is_none(), "target was found!");
 }
