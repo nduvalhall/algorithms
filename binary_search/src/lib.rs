a67ee87d68464541a0a7ec0a83b9284f0e8a30ea
nduvalhall <nduvalhall@gmail.com>
2024-04-01 14:05:31 +0100

    implemented binary search algorith

--- binary_search/src/lib.rs
+++ binary_search/src/lib.rs
@@ -1,39 +1,34 @@
+/// Finds the index of a target in an array via binary search
 #[allow(dead_code)]
-fn binary_search(target: usize, slice: &[usize]) -> Option<usize> {
-    if slice.len() == 0 {
-        return None;
-    }
+fn binary_search(target: i32, slice: &[i32]) -> Option<usize> {
+    let mut bottom: i8 = 0;
+    let mut top: i8 = slice.len() as i8 - 1;
 
-    let current_slice = slice;
-    let mut index = (current_slice.len() - 1) / 2;
+    while bottom <= top {
+        let mid = ((top - bottom) / 2) + bottom;
+        let current = slice[mid as usize];
 
-    loop {
-        if current_slice.len() == 1 {
-            if current_slice[0] == target {
-                return Some(0);
-            } else {
-                return None;
-            }
+        if current == target {
+            return Some(mid as usize);
         }
 
-        if current_slice[index] == target {
-            return Some(index);
+        if current > target {
+            top = mid - 1;
+            continue;
         }
 
-        if current_slice[index] > target {
-            let current_slice = &current_slice[0..index];
-            index = (current_slice.len() - 1) / 2;
-        }
-
-        if current_slice[index] < target {
-            let current_slice = &current_slice[index..current_slice.len()];
-            index = (current_slice.len() - 1) / 2;
+        if current < target {
+            bottom = mid + 1;
+            continue;
         }
     }
+
+    None
 }
 
+/// Finds the index of a target in an array via linear search
 #[allow(dead_code)]
-fn linear_search(target: usize, slice: &[usize]) -> Option<usize> {
+fn linear_search(target: i32, slice: &[i32]) -> Option<usize> {
     for (index, &item) in slice.iter().enumerate() {
         if target == item {
             return Some(index);
@@ -44,7 +39,7 @@ fn linear_search(target: usize, slice: &[usize]) -> Option<usize> {
 
 #[test]
 fn even_length_array() {
-    let slice = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] as [usize; 10];
+    let slice = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
     for target in slice {
         let lin = linear_search(target, &slice);
@@ -60,7 +55,7 @@ fn even_length_array() {
 
 #[test]
 fn odd_length_array() {
-    let slice = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110] as [usize; 11];
+    let slice = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110];
 
     for target in slice {
         let lin = linear_search(target, &slice);
@@ -76,7 +71,7 @@ fn odd_length_array() {
 
 #[test]
 fn one_item_array() {
-    let slice = [10] as [usize; 1];
+    let slice = [10];
 
     let target = 10;
     let lin = linear_search(target, &slice);
